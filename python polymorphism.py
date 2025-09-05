33f366501817ab9d500be85064941feed592f129
Tobioladipo01 <tobioladipo004@gmail.com>
2025-09-05 14:00:13 +0100

    python polymorphism

--- python polymorphism.py
+++ python polymorphism.py
@@ -27,3 +27,37 @@ Plane1 = Plane ("Boeing", "747")
 
 for x in (Car1, Boat1, Plane1):
      x.move()
+#this for loop possible because of polymorphism
+#for loop can also be written like this
+vehicle = [Car1, Boat1, Plane1]
+for x in vehicle:
+     x.move()
+
+
+
+
+class Vehicle:
+     def __init__ (self, brand, model):
+          self.brand = brand
+          self.model = model
+     def move(self):
+        print("Move!")
+
+class Car(Vehicle):
+     pass
+
+class Boat(Vehicle):
+     def move(self):
+          print ("Sail!")
+
+class Plane(Vehicle):
+     def move(self):
+          print("Fly!")
+
+c1= Car ("Toyota", "Camry")
+b1= Boat ("Ibiza", "idk")
+p1= Plane("Emirates", "C130")
+
+print(c1.model)
+for x in (c1, b1, p1):
+     x.move()
