6ad4d5fe6283dec37ed4760661d07e433ce7993f
Tobioladipo01 <tobioladipo004@gmail.com>
2025-09-07 21:08:48 +0100

    playing around with python math

---
+++ python math.py
@@ -0,0 +1,26 @@
+#python has some built in math function you can use without importing the math module for simple airthmetic tasks
+
+y=min(2,3,4,6,5)
+x= max(3,4,1,45,6)
+print(y)
+print(x)
+
+#abs return the absolute positive integer
+z=abs(-2.76)
+print(z)
+
+# pow (x,y) prints x raised to the power of y
+a= pow(10, 2)
+print(a)
+
+import math
+b=math.sqrt(25)
+print(b)
+
+c=math.ceil(1.4)  #rounds up a number to its nearest whole number
+d=math.floor(1.4)  # rounds down a number to its neares whole number
+print(c)
+print(d)
+
+e=math.pi  #returns the value for pi
+print(e)
