e119dcafd24ad9858c40440e526874b46640f331
Tobioladipo01 <tobioladipo004@gmail.com>
2025-09-10 22:24:18 +0100

    Understanding regex

--- RegEx.py
+++ RegEx.py
@@ -1,8 +1,19 @@
-# used to perform search in regular expressions. you start by importing the re module
+# used to perform search in regular expressions. you start by importing the re module( +, * and {} are quantifiers used to search for the number of occurances)
+# \d will search for a single numerical digit.   \d\d two consequtive numerical digit   \s for space  \w for all digits and letter and _
+
 
 import re
 txt = "The raain in spain"
 x= re.search("^The.raain$", txt)
 
 print(x)
+
+import re
+names = ["Tobi Oladipo", "Murtala Mohammad", "Williams Kumuyi", "Goodluck Jonathan", "Elon Musk", "john"]
+#find people with first and last name only
+regex= "^\w+ \w+$"
+for name in names:
+    print (re.search(regex, names))
+
+
 
