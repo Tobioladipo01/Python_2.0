f4dcb68469697302d535aacc7c01fa0deb7d0f69
Tobioladipo01 <tobioladipo004@gmail.com>
2025-09-11 15:40:05 +0100

    Final regex commit

--- RegEx.py
+++ RegEx.py
@@ -13,7 +13,35 @@ names = ["Tobi Oladipo", "Murtala Mohammad", "Williams Kumuyi", "Goodluck Jonath
 #find people with first and last name only
 regex= "^\w+ \w+$"
 for name in names:
-    print (re.search(regex, names))
+    result= (re.search(regex, name))
+    if result:
+        print(name)
+
+
+regex2= "^[a-zA-Z]+$"
+for n in names:
+    if re.search(regex2, n):
+        print(n)
+    
+
+'''regex3 = "[a-zA-Z0-9]+@[a-zA-z]+\.(com|edu|net)"
+userinput= input()
+if re.search(regex3, userinput):
+    print ("Valid email")
+else:
+    print("Input a valid email")'''
+
+#.findall returns all your results in a list
+#.split cuts out that particular thing youre looking for
+quote = "There's only one thing i hate more than lying: Skin milk. which is water that's lying about being milk. -Ron Swanson"
+pattern= "milk"
+print (re.search(pattern, quote))
+print (re.findall(pattern, quote))
+print (re.split(pattern, quote))
+print(re.sub("hate", "love", quote))
+
+
+
 
 
 
