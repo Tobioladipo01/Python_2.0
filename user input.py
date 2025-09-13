3d8ee3e4740f2a023d39645c3beb71a68a543f2a
Tobioladipo01 <tobioladipo004@gmail.com>
2025-09-13 08:39:43 +0100

    python user input

---
+++ user input.py
@@ -0,0 +1,17 @@
+'''print ("Enter your name:")
+name = input()
+print (f"Weclome {name}, hope you enjoy your stay")'''
+
+
+'''nam = input ("Enter your name: ")
+print (f"Hello, {nam}")
+fav1 = input ("What's your favourite color?")
+fav2 = input ("What is your favourite animal?")
+fav3 = input ("What is your favourite number?")
+print (f"do you want {fav1} {fav2} with {fav3} legs?")'''
+
+
+import math
+x = float( input ("Input a number above 20:"))
+y= math.sqrt (x)
+print(y)
